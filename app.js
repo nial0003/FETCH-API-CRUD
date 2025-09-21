48ab89f573235cc420888f4242aeaf53964f9676
nial0003 <nial0003@stud.kea.dk>
2025-09-21 22:01:39 +0200

    Split the renderUsers up into different functions to add readability. Also split the part of the renderTableRows() that handles nested JSON objects into its own function for hopefully better readability.

--- app.js
+++ app.js
@@ -1,73 +1,80 @@
-
 const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
 initApp();
 
-async function initApp(){
-    const data = await fetchData();
-    renderUsers(data);
+async function initApp() {
+  const data = await fetchData();
+  renderUsers(data);
 }
 
-async function fetchData(){
-    try{
-        const response = await fetch(USERS_URL);
+async function fetchData() {
+  try {
+    const response = await fetch(USERS_URL);
 
-        if(!response.ok){
-            throw new Error("Couldn't fetch users");
-        }
-
-        const jsonData = await response.json();
-
-        console.log(jsonData);
-        console.log(Array.isArray(jsonData));
-        return jsonData;
-    } catch(error){
-        console.error(error);
+    if (!response.ok) {
+      throw new Error("Couldn't fetch users");
     }
+
+    const jsonData = await response.json();
+
+    console.log(jsonData);
+    console.log(Array.isArray(jsonData));
+    return jsonData;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function renderUsers(users){
-    const table = document.querySelector("#userTable");
-    const headerRow = document.createElement("tr");
-    const keys = Object.keys(users[0]);
+function renderUsers(users) {
+  const table = document.querySelector("#userTable");
+  const keys = Object.keys(users[0]);
+
+  renderTableHead(table, users, keys);
+  
+  renderTableRows(table, users, keys);
+}
+
+function renderTableHead(table, users, keys) {
+  const headerRow = document.createElement("tr");
+  for (const key of keys) {
+    const tableHead = document.createElement("th");
+    tableHead.textContent = key;
+    headerRow.appendChild(tableHead);
+  }
+  table.appendChild(headerRow);
+}
+
+function renderTableRows(table, users, keys) {
+  var id = 1;
+
+  for (const user of users) {
+    const tableRow = document.createElement("tr");
+    tableRow.id = "data-user-id-" + id++;
+
     for (const key of keys) {
-        const tableHead = document.createElement("th");
-        tableHead.textContent = key;
-        headerRow.appendChild(tableHead);
-    }
-    table.appendChild(headerRow);
+      const tableData = document.createElement("td");
 
-    var id = 1;
-    for (const user of users) {
-        const tableRow = document.createElement("tr");
+      if (typeof user[key] === "object" && user[key] !== null) {
         
-        tableRow.id = "data-user-id-" + id++; 
-        for (const key of keys) {
-            const tableData = document.createElement("td");
+        tableData.textContent = handleNestedJsonObjects(user, key);
 
-            if(typeof user[key] === "object" && user[key] !== null){
-            let nested = [];
-
-            for (const nestedKey of Object.keys(user[key])) {
-                const value = user[key][nestedKey]
-                if (typeof value !== "object" || value === null) {
-          nested.push(`${value}`);
-        }
-            }
-            tableData.textContent = nested.join(", ")
-            } else{
-                  tableData.textContent = user[key];                   
-            }
-            tableRow.appendChild(tableData); 
-        }
-        table.appendChild(tableRow);
+      } else {
+        tableData.textContent = user[key];
+      }
+      tableRow.appendChild(tableData);
     }
+    table.appendChild(tableRow);
+  }
 }
 
-function renderTableHead(users){
+function handleNestedJsonObjects(user, key){
+    let nested = [];
 
-}
-
-function checkIfNestedObject(key, user){
-    
+        for (const nestedKey of Object.keys(user[key])) {
+          const value = user[key][nestedKey];
+          if (typeof value !== "object" || value === null) {
+            nested.push(`${value}`);
+          }
+        }
+        return nested.join(", ");
 }
